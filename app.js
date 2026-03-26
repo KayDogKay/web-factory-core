3d6c012b138ff221181f2a1f1592f3530244a4f6
KayDogKay <praktisch.kad@gmail.com>
2026-03-26 16:07:19 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+{
+  "firmenname": "Müller Haustechnik GmbH",
+  "beschreibung": "Seit über 25 Jahren Ihr zuverlässiger Partner für Sanitär, Heizung und erneuerbare Energien im Großraum Erfurt. Wir stehen für handwerkliche Qualität, transparente Preise und schnelle Reaktionszeiten.",
+  "leistungen": [
+    "Sanitärinstallation & Wartung",
+    "Heizungsanlagen & Modernisierung",
+    "Wärmepumpen & Solarthermie",
+    "Badsanierung & Umbauten",
+    "Notdienst 24/7",
+    "Energieberatung & Förderanträge"
+  ]
+}
